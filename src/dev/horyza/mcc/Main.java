1cdb9b8bfce33b89122254bf59a4bc8cc4559108
Joe Horyza <joseph.mn27@yahoo.com>
2021-05-04 13:31:07 -0500

    Refactored GUI and added additional panels

--- src/dev/horyza/mcc/Main.java
+++ src/dev/horyza/mcc/Main.java
@@ -1,13 +1,22 @@
 package dev.horyza.mcc;
 
-import dev.horyza.mcc.ui.Gui;
+import java.awt.EventQueue;
+
+import dev.horyza.mcc.ui.GUI;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Gui gui = new Gui();
-		
-		gui.display();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI frame = new GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
